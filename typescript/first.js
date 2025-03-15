1a0f5df944f394d7f7b0720881736c742689c435
Ritesh <riteshtiwari3112003@gmail.com>
2025-03-15 23:47:27 +0530

    this is my first typescript introduction program

---
+++ typescript/first.js
@@ -0,0 +1,4 @@
+var add = function (a, b) {
+    return (a + b);
+};
+console.log("the sum is ".concat(add(4, 5), " "));
---
+++ typescript/first.ts
@@ -0,0 +1,4 @@
+const add=(a,b)=>{
+    return (a+b)
+};
+console.log(`the sum is ${add(4,5)} `);
